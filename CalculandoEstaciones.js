617688d69664f6a77a95eedf26e41eed25517b85
davidventurag <davidventura201142593a@gmail.com>
2023-02-13 16:43:58 -0600

    Tarea estaciones

---
+++ CalculandoEstaciones.js
@@ -0,0 +1,24 @@
+let mes = prompt("Ingrese el número de un mes");
+if(mes > 12)
+{
+alert("Valor invalido");
+}
+else{
+	if(mes == 12 || mes == 1 || mes == 2)
+	{
+		alert("Es Invierno");
+	}
+	else if(mes >= 3 && mes <= 5)
+	{
+		alert("Es Primavera");
+	}
+	else if(mes >= 6 && mes <= 8)
+	{
+		alert("Es Verano");
+	}
+	else if(mes >= 9 && mes <= 11)
+	{
+		alert("Es Verano");
+	}
+}
+alert("David Josue Ventura Gonzalez carne:23002611");
